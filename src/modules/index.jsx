3ebb50666ced85b04ee876b655b6628e9db81702
Kirill Ilinsky <kirill.ilinsky@gmail.com>
2021-06-21 17:11:44 +0300

    clean folders

--- src/modules/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as Years } from "./years";
-export { default as Months } from "./months";
-export { default as Days } from "./days";
-export { default as Time } from "./time";
-export { default as Presets } from "./presets";
