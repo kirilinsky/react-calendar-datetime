bf7614b229e8463e87d0c7d4f235282a034e18b8
Kirill Ilinsky <kirill.ilinsky@gmail.com>
2021-06-22 10:29:48 +0300

    better stories

--- src/stories/Calendar.stories.js
+++ src/stories/Calendar.stories.js
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
 import Calendar from '../Calendar'
+import { Time } from '../modules'
+import moment from 'moment'
 
 const stories = storiesOf('Calendar react', module)
 
-
+stories.add('Base', () => {
+    const [date, setDate] = useState(new Date())
+    return <>
+        <h2>current time: {moment(date).format('DD.MM.YYYY')}</h2>
+        <Calendar date={date} onChangeDate={setDate} />
+    </>
+})
 
 stories.add('With time picker', () => {
-    return <Calendar time />
+    const [date, setDate] = useState(new Date())
+    return <>
+        <h2>current time: {moment(date).format('DD.MM.YYYY HH:mm')}</h2>
+        <Calendar date={date} onChangeDate={setDate} time />
+    </>
 })
 
 stories.add('With presets  ', () => {
-    return <Calendar presets />
+    const [date, setDate] = useState(new Date())
+    return <>
+        <h2>current time: {moment(date).format('DD.MM.YYYY')}</h2>
+        <Calendar date={date} onChangeDate={setDate} presets />
+    </>
 })
 
-stories.add('WIth presets  and time picker', () => {
-    return <Calendar time presets />
+stories.add('With presets  and time picker', () => {
+    const [date, setDate] = useState(new Date())
+    return <>
+        <h2>current time: {moment(date).format('DD.MM.YYYY HH:mm')}</h2>
+        <Calendar date={date} onChangeDate={setDate} presets time />
+    </>
 })
 
 stories.add('International', () => {
-    return <Calendar locale="uk" presets />
+    const [date, setDate] = useState(new Date())
+    return <>
+        <h2>current time: {moment(date).format('DD.MM.YYYY')}</h2>
+        <Calendar date={date} locale="uk" onChangeDate={setDate} presets />
+    </>
 })
